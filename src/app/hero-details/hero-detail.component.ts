6a1550aa1ea503075f25a8beff4e0f40117c15e5
rozsabence <robence@gmail.com>
2018-11-26 15:45:30 +0100

    extend CRUD with httpClient

--- src/app/hero-details/hero-detail.component.ts
+++ src/app/hero-details/hero-detail.component.ts
@@ -3,6 +3,7 @@ import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from '../hero.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
@@ -32,4 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
